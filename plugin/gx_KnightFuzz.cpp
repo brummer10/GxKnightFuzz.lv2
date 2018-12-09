88856228bb2e43ed3e7bd47c1c01310e59820cca
brummer10 <brummer-@web.de>
2018-12-09 19:13:24 +0100

    fix bypass state on automation

--- plugin/gx_KnightFuzz.cpp
+++ plugin/gx_KnightFuzz.cpp
@@ -123,7 +123,7 @@ Gx_KnightFuzz_::Gx_KnightFuzz_() :
   input(NULL),
   KnightFuzz(KnightFuzz::plugin()),
   bypass(0),
-  bypass_(0),
+  bypass_(2),
   needs_ramp_down(false),
   needs_ramp_up(false),
   bypassed(false) {};
